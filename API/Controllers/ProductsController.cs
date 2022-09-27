e0f84a554da1b23a171d444638710e10f515f4a3
tuong truong <vinhtuong.contact@gmail.com>
2022-09-27 16:59:44 +1000

    first commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+            
+        }
+        [HttpGet]
+        public async Task<ActionResult<Product>> GetProducts()
+        {
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _repo.GetProductByIdAsync(id);
+        }
+    }
+}
